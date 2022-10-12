712152826b297c64187dc2aed1ebd08e6ea4fa2c
Irfanwani <irfanwani347@gmail.com>
2022-10-12 17:36:25 +0530

    added dark theme

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,15 @@ const Page = () => {
   return (
     <Container>
       <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
-        Hello, I&apos;m a full-stack developer based in Kashmir, India!
+        Hello, I&apos;m a full-stack web developer and mobile app developer
+        based in Kashmir, India!
       </Box>
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
-            <Heading as='h2' variant='page-title'>
-                Irfan Wani
-            </Heading>
-            <p>
-                Programmer (Web develpoer / App developer)
-            </p>
-
-            
-
+          <Heading as="h2" variant="page-title">
+            Irfan Wani
+          </Heading>
+          <p>Programmer (Web develpoer / App developer)</p>
         </Box>
       </Box>
     </Container>
